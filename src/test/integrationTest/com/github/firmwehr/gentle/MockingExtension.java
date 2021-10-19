fefe8f6a16e7069ae674c075480842eb963904ae
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-10-19 23:57:44 +0200

    Add integration tests

---
+++ src/test/integrationTest/com/github/firmwehr/gentle/MockingExtension.java
@@ -0,0 +1,28 @@
+package com.github.firmwehr.gentle;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+public class MockingExtension implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+	
+	private static boolean started = false;
+	
+	@Override
+	public void beforeAll(ExtensionContext context) throws IOException, InterruptedException {
+		if (!started) {
+			started = true;
+			context.getRoot().getStore(GLOBAL).put(getClass().getSimpleName(), this);
+			int exitCode = new ProcessBuilder("sh", "-c", "./build").inheritIO().start().waitFor();
+			assertThat(exitCode).isZero();
+		}
+	}
+	
+	@Override
+	public void close() {
+	}
+}
