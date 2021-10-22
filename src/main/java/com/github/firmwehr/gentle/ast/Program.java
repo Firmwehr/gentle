c7916a327ebfddae81bacb6bec795e638faf9b86
Paul Brinkmeier <hallo@pbrinkmeier.de>
2021-10-22 17:58:01 +0200

    Import List in Program

--- src/main/java/com/github/firmwehr/gentle/ast/Program.java
+++ src/main/java/com/github/firmwehr/gentle/ast/Program.java
@@ -1,4 +1,6 @@
 package com.github.firmwehr.gentle.ast;
 
+import java.util.List;
+
 public record Program<I>(SourcePosition position, List<ClassDeclaration<I>> classDeclarations) implements HasSourcePosition {
 }
