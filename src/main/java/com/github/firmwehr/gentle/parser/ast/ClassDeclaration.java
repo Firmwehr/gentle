34fe35fb74ff17f4c4bd9d41bf7bcddfe4267b2c
Joscha <joscha@plugh.de>
2021-10-26 22:31:27 +0200

    Fix class declaration AST node

--- src/main/java/com/github/firmwehr/gentle/parser/ast/ClassDeclaration.java
+++ src/main/java/com/github/firmwehr/gentle/parser/ast/ClassDeclaration.java
@@ -3,6 +3,7 @@ package com.github.firmwehr.gentle.parser.ast;
 import java.util.List;
 
 public record ClassDeclaration(
+	Ident name,
 	List<Field> fields,
 	List<Method> methods,
 	List<MainMethod> mainMethods
