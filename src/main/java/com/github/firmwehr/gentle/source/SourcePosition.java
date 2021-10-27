79ea8d9ec6e375a5bf55d70d4fa2b8fac16b4732
Chrisliebär <mail@chrisliebaer.de>
2021-10-27 15:58:23 +0200

    Fix wrong assumption about starting index in SourcePosition

--- src/main/java/com/github/firmwehr/gentle/source/SourcePosition.java
+++ src/main/java/com/github/firmwehr/gentle/source/SourcePosition.java
@@ -3,9 +3,9 @@ package com.github.firmwehr.gentle.source;
 import com.google.common.base.Preconditions;
 
 /**
- * The position of a single character in the source code file.
+ * The position of a single character in the source code file. (Please check for starting index!)
  *
- * @param offset total offset in characters from the start of the file (the first character has offset 1!)
+ * @param offset total offset in characters from the start of the file (the first character has offset 0!)
  * @param line line the character is in (starting with 1!)
  * @param column column the character is in (starting with 1!). Newlines and carriage returns are counted as the last
  * 	characters of their previous line.
@@ -16,9 +16,9 @@ public record SourcePosition(
 	int column
 ) {
 
-	// we use 1-based indexing
+	// we use 1-based indexing for lines and columns
 	public SourcePosition {
-		Preconditions.checkArgument(offset > 0, "offset must not be negative");
+		Preconditions.checkArgument(offset >= 0, "offset must not be negative");
 		Preconditions.checkArgument(line > 0, "line must not be smaller than 1");
 		Preconditions.checkArgument(column > 0, "column must not be smaller than 1");
 	}
