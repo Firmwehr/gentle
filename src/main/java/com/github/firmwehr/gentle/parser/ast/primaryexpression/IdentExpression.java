2aaa16290cf505d070f6d00513c26b16d8d140e9
Joscha <joscha@plugh.de>
2021-10-27 18:25:55 +0200

    Reformat code

--- src/main/java/com/github/firmwehr/gentle/parser/ast/primaryexpression/IdentExpression.java
+++ src/main/java/com/github/firmwehr/gentle/parser/ast/primaryexpression/IdentExpression.java
@@ -1,7 +1,6 @@
 package com.github.firmwehr.gentle.parser.ast.primaryexpression;
 
 import com.github.firmwehr.gentle.parser.ast.Ident;
-import com.github.firmwehr.gentle.parser.ast.expression.Expression;
 
 public record IdentExpression(Ident name) implements PrimaryExpression {
 }
--- src/main/java/com/github/firmwehr/gentle/parser/ast/primaryexpression/PrimaryExpression.java
+++ src/main/java/com/github/firmwehr/gentle/parser/ast/primaryexpression/PrimaryExpression.java
@@ -1,4 +1,6 @@
 package com.github.firmwehr.gentle.parser.ast.primaryexpression;
 
-public sealed interface PrimaryExpression permits NullExpression, BooleanLiteralExpression, IntegerLiteralExpression, IdentExpression, LocalMethodCallExpression, ThisExpression, JustAnExpression, NewObjectExpression, NewArrayExpression {
+public sealed interface PrimaryExpression
+	permits NullExpression, BooleanLiteralExpression, IntegerLiteralExpression, IdentExpression,
+	        LocalMethodCallExpression, ThisExpression, JustAnExpression, NewObjectExpression, NewArrayExpression {
 }
