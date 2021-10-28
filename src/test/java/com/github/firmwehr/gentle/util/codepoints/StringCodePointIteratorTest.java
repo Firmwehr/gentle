c28b82fcec8ac1661810175798996a698521a59d
SirYwell <hannesgreule@outlook.de>
2021-10-28 12:17:01 +0200

    Newline!

--- src/test/java/com/github/firmwehr/gentle/util/codepoints/StringCodePointIteratorTest.java
+++ src/test/java/com/github/firmwehr/gentle/util/codepoints/StringCodePointIteratorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCodePointIteratorTest {
 
