2448ee6309bc424a173a346cb23a9c3235cd6052
Chrisliebär <mail@chrisliebaer.de>
2021-10-29 15:57:43 +0200

    Remove support for line comments starting with //

--- src/main/java/com/github/firmwehr/gentle/lexer/tokens/TokenComment.java
+++ src/main/java/com/github/firmwehr/gentle/lexer/tokens/TokenComment.java
@@ -7,44 +7,25 @@ import com.github.firmwehr.gentle.source.SourceSpan;
 
 public record TokenComment(
 	SourceSpan sourceSpan,
-	String text,
-	CommentType type
+	String text
 ) implements Token {
 
 	public static TokenComment create(LexReader reader) throws LexerException {
-		var peek = reader.peek(2);
 		var startPos = reader.position(); // important to capture position before we consume entire comment
-		switch (peek) {
-			case "//" -> {
-				reader.expect("//");
-				return new TokenComment(new SourceSpan(startPos, reader.endPositionOfRead()), reader.readLine(),
-					CommentType.SINGLE_LINE);
-			}
-			case "/*" -> {
-				reader.expect("/*");
-				var s = reader.readUntil("*/", true);
-				s = s.substring(0, s.length() - 2); // strip trailing '*/'
-				return new TokenComment(new SourceSpan(startPos, reader.endPositionOfRead()), s, CommentType.STAR);
-			}
-			default -> throw new LexerException("could not detect comment start with either '//' or '/*'", reader);
-		}
+
+		reader.expect("/*");
+		var s = reader.readUntil("*/", true);
+		s = s.substring(0, s.length() - 2); // strip trailing '*/'
+		return new TokenComment(new SourceSpan(startPos, reader.endPositionOfRead()), s);
 	}
 
 	@Override
 	public String format() {
-		return switch (type) {
-			case STAR -> "/*" + text + "*/";
-			case SINGLE_LINE -> "//" + text;
-		};
+		return "/*" + text + "*/";
 	}
 
 	@Override
 	public TokenType tokenType() {
 		return TokenType.COMMENT;
-	}
-
-	public enum CommentType {
-		STAR,
-		SINGLE_LINE
 	}
 }
