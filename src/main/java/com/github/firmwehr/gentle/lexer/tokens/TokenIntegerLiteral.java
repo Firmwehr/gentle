11b1b5933751d0e232c03585cce13f3bee789013
Chrisliebär <mail@chrisliebaer.de>
2021-10-29 16:44:00 +0200

    Split integer literals with leading zero into two tokens instead of rejecting them

--- src/main/java/com/github/firmwehr/gentle/lexer/tokens/TokenIntegerLiteral.java
+++ src/main/java/com/github/firmwehr/gentle/lexer/tokens/TokenIntegerLiteral.java
@@ -15,7 +15,18 @@ public record TokenIntegerLiteral(
 	public static TokenIntegerLiteral create(LexReader reader) throws LexerException {
 		var startPos = reader.position();
 		try {
-			var str = reader.readUntilOrEndOfFile(v -> !Character.isDigit(v), false);
+			// edge case handling for iterals starting with 0, since these should be split into two tokens
+			var fork = reader.fork();
+			var str = fork.readUntilOrEndOfFile(v -> !Character.isDigit(v), false);
+			if (str.startsWith("0") && str.length() > 1) {
+				// only consume 0 and cause second token to be created with remainder
+				reader.consume();
+				return new TokenIntegerLiteral(new SourceSpan(startPos, reader.endPositionOfRead()), BigInteger.ZERO);
+			}
+
+			// advanced reader by consumed string
+			reader.expect(str);
+
 			if (str.startsWith("0") && str.length() > 1) {
 				throw new LexerException("leading zero is not allowed", reader);
 			}
