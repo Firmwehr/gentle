5981799932d083ca1d3cd87c6689537961dd0080
Joscha <joscha@plugh.de>
2021-11-05 11:27:41 +0100

    Add --print-ast flag

--- src/main/java/com/github/firmwehr/gentle/cli/CommandArguments.java
+++ src/main/java/com/github/firmwehr/gentle/cli/CommandArguments.java
@@ -17,8 +17,11 @@ public interface CommandArguments {
 	@Option(names = "--lextest", description = "Reads the given file and prints all tokens or aborts on first error")
 	boolean lextest();
 
-	@Option(names = "--parsetest", description = "Reads the given file and pretty-prints the parser AST")
+	@Option(names = "--parsetest", description = "Reads the given file and exits with 0 iff it is syntactically valid")
 	boolean parsetest();
+
+	@Option(names = "--print-ast", description = "Reads the given file and pretty-prints the parser AST")
+	boolean printAst();
 
 	@Parameter(index = 0, converter = ExistingFileConverter.class, description = "The file to operate on.",
 		paramLabel = "PATH")
