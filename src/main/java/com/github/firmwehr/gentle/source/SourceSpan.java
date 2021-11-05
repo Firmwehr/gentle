0d13e8c351546ce1a44a5762c5e1774067998030
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-11-05 17:16:53 +0100

    Address comment

--- src/main/java/com/github/firmwehr/gentle/source/SourceSpan.java
+++ src/main/java/com/github/firmwehr/gentle/source/SourceSpan.java
@@ -1,10 +1,11 @@
 package com.github.firmwehr.gentle.source;
 
 /**
- * A contiguous section of the source code file.
+ * A contiguous section of the source code file that extends from the character at {@code startOffset} to the character
+ * at {@code endOffset - 1}.
  *
- * @param startOffset the first character of the span (inclusive)
- * @param endOffset the last character of the span (inclusive)
+ * @param startOffset the start offset of the span (inclusive)
+ * @param endOffset the end offset of the span (exclusive)
  */
 public record SourceSpan(
 	int startOffset,
@@ -15,9 +16,5 @@ public record SourceSpan(
 		if (startOffset > endOffset) {
 			throw new IllegalArgumentException("start must not be later than end");
 		}
-	}
-
-	public String format() {
-		return startOffset + ".." + endOffset;
 	}
 }
