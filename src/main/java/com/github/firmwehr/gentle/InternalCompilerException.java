39f0d4cb3c44d0963dd1b8b580aa800d3e0ba2ba
Paul Brinkmeier <hallo@pbrinkmeier.de>
2021-11-29 14:37:59 +0100

    Add javadoc for ICE

--- src/main/java/com/github/firmwehr/gentle/InternalCompilerException.java
+++ src/main/java/com/github/firmwehr/gentle/InternalCompilerException.java
@@ -1,5 +1,9 @@
 package com.github.firmwehr.gentle;
 
+/**
+ * To be used in cases where a programmer mistake might lead to errors. For example this could be thrown when a branch
+ * can't be taken because of some violated data structure invariant.
+ */
 public class InternalCompilerException extends RuntimeException {
 
 	private final String message;
