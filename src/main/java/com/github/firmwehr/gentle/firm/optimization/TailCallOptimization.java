c5d94b4b6c970df647b769289ef4879be3d9404b
Paul Brinkmeier <hallo@pbrinkmeier.de>
2022-01-20 18:57:37 +0100

    Fix some bugs
    Keep memory projs alive even if all returns were deleted
    
    Fix order of argument phis in loop header

--- src/main/java/com/github/firmwehr/gentle/firm/optimization/TailCallOptimization.java
+++ src/main/java/com/github/firmwehr/gentle/firm/optimization/TailCallOptimization.java
@@ -4,6 +4,8 @@ import com.github.firmwehr.gentle.firm.Util;
 import com.github.firmwehr.gentle.output.Logger;
 import firm.BackEdges;
 import firm.Graph;
+import firm.Mode;
+import firm.bindings.binding_irgopt;
 import firm.nodes.Address;
 import firm.nodes.Anchor;
 import firm.nodes.Block;
@@ -42,21 +44,28 @@ public class TailCallOptimization extends NodeVisitor.Default {
 
 				List<Node> loopJumps = new ArrayList<>();
 
+				Node jumpFromStart = graph.newJmp(graph.getStartBlock());
+				loopJumps.add(jumpFromStart);
+
 				for (TailCall tailCall : tailCalls) {
 					Node jump = graph.newJmp(tailCall.ret().getBlock());
 					loopJumps.add(jump);
 				}
 
-				Node jumpFromStart = graph.newJmp(graph.getStartBlock());
-				loopJumps.add(jumpFromStart);
-
 				Node loopHeader = graph.newBlock(loopJumps.toArray(Node[]::new));
 				graph.keepAlive(loopHeader);
+
+				/* If the function does not branch, there are now two Jmps in the start block. We move the second one
+				to the loop header. */
+				for (int i = 1; i < loopJumps.size(); i++) {
+					if (loopJumps.get(i).getBlock().equals(graph.getStartBlock())) {
+						loopJumps.get(i).setBlock(loopHeader);
+					}
+				}
 
 				// TODO: Encapsulate in some method
 				Start start = graph.getStart();
 				Proj startMem = null;
-				// Proj startBp = null;
 				List<Proj> startArgs = new ArrayList<>();
 
 				for (BackEdges.Edge edge : BackEdges.getOuts(start)) {
@@ -67,10 +76,6 @@ public class TailCallOptimization extends NodeVisitor.Default {
 							case 0:
 								startMem = (Proj) edge.node;
 								break;
-							// TODO: Necessary?
-							// case 1:
-							// 	startBp = (Proj) edge.node;
-							// 	break;
 							case 2:
 								Util.outsStream(proj).forEach(node -> {
 									// Check for Anchor, ignore instance used for call
@@ -86,8 +91,7 @@ public class TailCallOptimization extends NodeVisitor.Default {
 				}
 
 				LOGGER.debugHeader("Moving code that depends on start node from start block to loop header...");
-				Set<Node> nodesToMove = new HashSet<>();
-				nodesToMove.addAll(successorsInBlock(startMem, graph.getStartBlock()));
+				Set<Node> nodesToMove = new HashSet<>(successorsInBlock(startMem, graph.getStartBlock()));
 				for (Proj arg : startArgs) {
 					nodesToMove.addAll(successorsInBlock(arg, graph.getStartBlock()));
 				}
@@ -100,12 +104,12 @@ public class TailCallOptimization extends NodeVisitor.Default {
 				LOGGER.debugHeader("Creating phis for arguments and start mem...");
 
 				assert startMem != null;
-				// assert startBp != null;
 
 				Phi memPhi =
 					generatePhi(loopHeader, startMem, tailCalls.stream().map(tc -> tc.call().getPred(0)).toList());
-				// TODO: Necessary?
-				// Phi bpPhi = generatePhi(loop, startBp, ...);
+				for (TailCall tc : tailCalls) {
+					graph.keepAlive(tc.call().getPred(0));
+				}
 
 				Map<Proj, Phi> argPhis = new HashMap<>();
 				for (Proj arg : startArgs) {
@@ -114,10 +118,12 @@ public class TailCallOptimization extends NodeVisitor.Default {
 						tailCalls.stream().map(tc -> tc.call().getPred(arg.getNum() + 2)).toList()));
 				}
 
-				LOGGER.debugHeader("Deleting call nodes...");
+				LOGGER.debugHeader("Deleting return nodes...");
 				for (TailCall tc : tailCalls) {
 					Graph.exchange(tc.ret(), graph.newBad(tc.ret().getMode()));
 				}
+
+				binding_irgopt.remove_bads(graph.ptr);
 
 				BackEdges.disable(graph);
 				return false;
@@ -154,28 +160,88 @@ public class TailCallOptimization extends NodeVisitor.Default {
 		return phi;
 	}
 
+	// TODO: Check edge cases, e.g. returning a local variable assigned in a block
 	private static List<TailCall> findTailCalls(Graph graph) {
 		LOGGER.debugHeader("Looking for tails calls in %d returns...", graph.getEndBlock().getPredCount());
 		List<TailCall> tailCalls = new ArrayList<>();
-		for (Node endPred : graph.getEndBlock().getPreds()) {
+		Block end = graph.getEndBlock();
+		examineReturns:
+		for (Node endPred : end.getPreds()) {
 			LOGGER.debug("Examining node %s", endPred);
 			assert endPred instanceof Return;
 			Return ret = (Return) endPred;
 
-			// First ret pred is a memory proj, skip that
-			// Check whether last mem before return came from a call
-			if (!(ret.getPred(0).getPred(0) instanceof Call)) {
+			/*
+			tail calls looks something like this:
+						+---------+
+						| Call    | <-- call
+						+---------+
+			              ^     ^
+			              | 0   `--------\
+			              |              | 0
+			              |              |
+			            +----------+   +-----------------+
+			memProj --> | Proj M M |   | Proj T T_result | <-- tupleProj_i
+			            +----------+   +-----------------+
+			              ^              ^                                        ...
+			              |              |
+			              |              | 0
+			              |            +----------+
+			              | 0          | Proj _ 0 | <-- resultProj_i
+			              |            +----------+
+			              |              |
+			              |  /-----------´
+			              |  | 1
+			              |  |
+						+--------+
+						| Return | <-- ret
+						+--------+
+			The underscore _ in the Proj input 1 of the Return is the return type of the Call (Bu, Is or P).
+			For void calls the Return node only has the memory input.
+			We need to check these properties for each Return ret:
+			1) input 0 of ret is a Proj M memProj whose input 0 is a Call call
+			2) call actually calls graph and not some other function
+			3) for i in [1..ret.getPredCount()]: input i of ret is a Proj whose input 0 is a Proj T whose input 0 is
+			call
+			In gentle we could assume that ret.getPredCount() is either 1 (void) or 2 (boolean/int). In my opinion,
+			the general solution is nicer though.
+			 */
+
+			// 1) Check direct mem dependency, find call
+			if (!(ret.getPred(0) instanceof Proj memProj)) {
+				continue;
+			}
+			assert memProj.getMode().equals(Mode.getM());
+			if (!(memProj.getPred(0) instanceof Call call)) {
 				continue;
 			}
 
-			Call retCall = (Call) ret.getPred(0).getPred(0);
-			Address retCallFun = (Address) retCall.getPred(1);
-
-			if (retCallFun.getEntity().equals(graph.getEntity())) {
-				LOGGER.debug("%s is in a fact a tail call", endPred);
-				tailCalls.add(new TailCall(ret, retCall));
+			// 2) Ensure that this is a recursive call
+			Address calledFunction = (Address) call.getPred(1);
+			if (!calledFunction.getEntity().equals(graph.getEntity())) {
+				continue;
 			}
+
+			// 3) Check whether only results of call are returned
+			for (int i = 1; i < ret.getPredCount(); i++) {
+				if (!(ret.getPred(i) instanceof Proj resultProj)) {
+					continue examineReturns;
+				}
+				if (!(resultProj.getPred(0) instanceof Proj tupleProj)) {
+					continue examineReturns;
+				}
+				assert tupleProj.getMode().equals(Mode.getT());
+				if (!tupleProj.getPred(0).equals(call)) {
+					continue examineReturns;
+				}
+
+				LOGGER.debug("[%d] resultProj: %s tupleProj: %s", i, resultProj, tupleProj);
+			}
+
+			LOGGER.debug("%s is in a fact a tail call", endPred);
+			tailCalls.add(new TailCall(ret, call));
 		}
+
 		return tailCalls;
 	}
 
